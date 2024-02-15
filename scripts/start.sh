e147fa73bb5ef064309e093d96c2d5fff04cdcc4
Brendon Hablutzel <booklvrbren@gmail.com>
2024-02-15 14:45:21 -0500

    change string equality checking in bash script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-if [ "${1}" == "" ]; then
+if [ "${1}" = "" ]; then
     echo "ERROR: an option (create, read, or delete) is required as the first argument"
     exit 1
 fi
 
-if [ "${1}" == "create" ]; then
+if [ "${1}" = "create" ]; then
     python3 create.py $2 $3 $4 $5
-    exit
+    exit 0
 fi
 
-if [ "${1}" == "read" ]; then
+if [ "${1}" = "read" ]; then
     python3 read.py $2
-    exit
+    exit 0
 fi
 
-if [ "${1}" == "update" ]; then
+if [ "${1}" = "update" ]; then
     python3 update.py $2 $3 $4 $5
-    exit
+    exit 0
 fi
 
-if [ "${1}" == "delete" ]; then
+if [ "${1}" = "delete" ]; then
     python3 delete.py $2
-    exit
+    exit 0
 fi
 
 echo "ERROR: invalid option $1, expected create, read, or delete"
+exit 1
+
